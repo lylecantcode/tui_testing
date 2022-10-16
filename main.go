83e5eee8d9a2a6be0a8af1157f886a46da3a77b4
lylecantcode <lylejack75@gmail.com>
2022-10-16 03:31:57 +0100

    added lots of additional comments, removed some redundant and experimental code, cleaned up some formatting issues

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	// "github.com/charmbracelet/lipgloss"
+	// "github.com/charmbracelet/lipgloss" // package for the future, for style changes.
 	_ "github.com/mattn/go-sqlite3"
 	"os"
 	"time"
@@ -15,31 +15,27 @@ var db = dbStartUp()
 var TODAY string = time.Now().Format("02-01-2006")
 
 type model struct {
-	choices   []string            // items on the to-do list
-	cursor    int              // which to-do list item our cursor is pointing at
-	selected  map[int]struct{} // which to-do items are selected
-	addingNew textinput.Model  //testing
+	choices   []string         // items on the to-do list
+	cursor    int              // which list item our cursor is pointing at
+	selected  map[int]struct{} // which items are selected
+	addingNew textinput.Model  // used to add new values
 }
 
 func initialModel() model {
 	items := model{selected: make(map[int]struct{})}
 	values := getValues()
-	new := textinput.New()
-	new.Prompt = ""
-	new.Placeholder = "Add New Item"
-	//new.Focus() // does this lock it?
-	new.CharLimit = 20 // was 156
-	new.Width = 10     // was 20
+	insertModel := textinput.New()
+	insertModel.Placeholder = "Add New Item"
+	insertModel.Prompt = ""    // we do not want an additional prompt for the "add new item"
+	insertModel.CharLimit = 20 // no real reason for this limitation, can experiment
+	insertModel.Width = 10     // no real reason for this limitation, can experiment
 
 	if len(values) == 0 {
-		items.choices = []string{"Protein Shake", "Creatine", "Vitamin D", "Cuddles", "Pizza", "Chocolate", "Bananas"}
+		items.choices = []string{"Protein Shake", "Creatine", "Vitamin D", "Cheese", "Pizza", "Chocolate", "Bananas"}
 	} else {
 		items.choices = values
 	}
-	// A map which indicates which choices are selected. We're using
-	// the  map like a mathematical set. The keys refer to the indexes
-	// of the `choices` slice, above.
-	items.addingNew = new
+	items.addingNew = insertModel
 
 	for i := 0; i < len(items.choices); i++ {
 		items.addItems(items.choices[i], false)
@@ -49,8 +45,7 @@ func initialModel() model {
 }
 
 func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
-	return textinput.Blink // nil
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -68,26 +63,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 
-		// The "up" and "k" keys move the cursor up
-		case "up": //, "k":
+		// The "up" key moves the cursor up
+		case "up":
 			if m.cursor > 0 {
 				m.cursor--
 			}
 
-		// The "down" and "j" keys move the cursor down
-		case "down": //, "j":
+		// The "down" key moves the cursor down
+		case "down":
 			if m.cursor < len(m.choices) { //-1 {
 				m.cursor++
 			}
 
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter": //, " ":
-			comparison := textinput.New()
-			comparison.Prompt = ""
-			comparison.Placeholder = "Please type value here"
-			// comparison.Focus()
-
+		// The "enter" key toggles the selected state for the item that the cursor is pointing at.
+		// if pointing at addNew, this then allows a new value to be typed in.
+		// de-focus to stop input and update DB selection boolean.
+		case "enter":
 			if m.cursor < len(m.choices) {
 				m.addingNew.Blur()
 				m.checkItem(m.cursor)
@@ -97,8 +88,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				} else {
 					m.selected[m.cursor] = struct{}{}
 				}
-			} else { //if m.choices[m.cursor-1] != comparison.View() {
+				// if out of range, then we are on the "addNew selection."
+			} else {
 				_, ok := m.selected[m.cursor]
+				// logic to toggle the selection and also "focus" when selected to allow for input
 				if ok {
 					m.addNewItem()
 				} else {
@@ -111,19 +104,20 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.choices = deleteChoice(m.choices, m.cursor)
 		}
 	}
-	cmd = m.updateInputs(msg)
-	// if m.cursor != len(m.choices) && m.addingNew.Value() != "Add New Item" {
-	// 	m.choices = append(m.choices, m.addingNew.Value())
-	// 	m.addingNew.Reset()
-	// }
-
+	cmd = m.updateInputs(msg) // used to drive the addNewItem functionality.
 	return m, cmd
 }
 
+// remove item from array by appending the other elements of the array.
 func deleteChoice(s []string, index int) []string {
 	return append(s[:index], s[index+1:]...)
 }
 
+// insert new item to choices, from the addingNew model
+// unfocus to stop further input
+// reset addingNew back to default state
+// delete selection of addNew, reselect newly inserted item
+// add new item to DB
 func (m *model) addNewItem() {
 	newItem := m.addingNew.Value()
 	m.choices = append(m.choices, newItem)
@@ -134,6 +128,7 @@ func (m *model) addNewItem() {
 	m.addItems(newItem, true)
 }
 
+// handles typing once focused.
 func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
 	var cmd tea.Cmd
 	m.addingNew, cmd = m.addingNew.Update(msg)
@@ -142,10 +137,10 @@ func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
 
 func (m model) View() string {
 	// The header
-	s := "What do you have to have today?\n\n"
+	s := "What do you need?\n\n"
 
 	// Iterate over our choices
-	for i := 0; i <= len(m.choices); i++ { //, choice := range m.choices {
+	for i := 0; i <= len(m.choices); i++ {
 
 		// Is the cursor pointing at this choice?
 		cursor := " " // no cursor
@@ -161,20 +156,12 @@ func (m model) View() string {
 
 		// Render the row
 		if i < len(m.choices) {
-			//var typeInterface interface{} = m.choices
-			//if _, ok := m.choices[i].(string); ok {
-				s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, m.choices[i])
-			//} else {
-			//	s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, m.choices[i]) //m.choices[i].Value())
-			//}
+			s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, m.choices[i])
 		} else {
+			// Render the addingNew row, both in "adding" state and in default state.
 			s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, m.addingNew.View())
 		}
 	}
-
-	// } else {
-	// 	s += "oops"
-	// }
 	// The footer
 	s += "\nPress Delete to delete an item. \nPress q to quit.\n"
 	return s
@@ -188,6 +175,7 @@ func main() {
 	}
 }
 
+// create a basic database table if it doesn't exist.
 func dbStartUp() *sql.DB {
 	database, _ :=
 		sql.Open("sqlite3", "./list.db")
@@ -199,40 +187,33 @@ func dbStartUp() *sql.DB {
 }
 
 func (m model) addItems(item string, checked bool) {
-		statement, _ :=
-			db.Prepare("INSERT INTO list (item, date, checked) VALUES (?, ?, ?)")
-		statement.Exec(item, TODAY, checked)	
+	statement, _ :=
+		db.Prepare("INSERT INTO list (item, date, checked) VALUES (?, ?, ?)")
+	statement.Exec(item, TODAY, checked)
 }
 
 func (m model) deleteItems(item string) {
-		statement, _ :=
-			db.Prepare("DELETE FROM list WHERE item = ?")
-		statement.Exec(item)
+	statement, _ :=
+		db.Prepare("DELETE FROM list WHERE item = ?")
+	statement.Exec(item)
 }
 
 func (m model) checkItem(i int) {
-	if m.choices[i] == "Add New Item" {
-		m.choices[i] = "lol"
-		fmt.Scanln(&m.choices[i])
-		// m.choices = append(m.choices, "Add New Item") // corrupts rest of it
-	}
-
 	statement, _ :=
 		db.Prepare("UPDATE list SET checked = NOT checked WHERE item = ?")
 	statement.Exec(m.choices[i])
 }
 
-func getValues() []string { //map[string]bool {
-	// itemMap := make(map[string]bool)
+// prepopulate the choices if there is an existing database, rather than using default values
+// i.e. memory between sessions.
+func getValues() []string {
 	var item string
-	// var status bool
 	itemArray := []string{}
 
 	rows, _ :=
-		db.Query("SELECT item FROM list") //, checked FROM list")
+		db.Query("SELECT item FROM list")
 	for rows.Next() {
-		rows.Scan(&item) //, &status)
-		//itemMap[item] = status
+		rows.Scan(&item)
 		itemArray = append(itemArray, item)
 	}
 	return itemArray
